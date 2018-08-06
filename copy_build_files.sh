2c0a7ff8626ab783002697761efb807a0d0ab5fc
Jakub <jakub.mojsiejuk@gmail.com>
2018-08-06 10:52:51 +0100

    build copy strategy update

--- copy_build_files.sh
+++ copy_build_files.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 mkdir singularity_builds
-cp /*/Singularity.* singularity_builds
-cd singularity_builds
-cp ./kafka_prod/kafka_prod .
-cp ./kafka_cont/kafka_serv .
-cp ./kafka_cons/kafka_cons .
-cp ./spark_cont/spark .
-cp ./cassandra_cont/cassandra .
+cp ./kafka_prod/kafka_prod singularity_builds 
+cp ./kafka_cont/kafka_serv singularity_builds
+cp ./kafka_cons/kafka_cons singularity_builds
+cp ./spark_cont/spark singularity_builds 
+cp ./cassandra_cont/cassandra singularity_builds
 
