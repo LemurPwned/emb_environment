c9fb447ae0d09bbc048d7d2d772315b0b7014e2d
Jakub <jakub.mojsiejuk@gmail.com>
2018-08-09 17:15:30 +0100

    changing script from wurstmeister to work as a standalone

--- zookeeper_cont/start-zk.sh
+++ zookeeper_cont/start-zk.sh
@@ -1,4 +1,4 @@
 sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $HOME/$ZK_HOME/conf/log4j.properties
-sed -i -r 's|#autopurge|autopurge|g' $HOME/$ZK_HOME/conf/zoo.cfg
+sed -i -r 's|#autopurge|autopurge|g' $HOME/$ZK_HOME/conf/zoo_sample.cfg
 
 $HOME/zookeeper-${ZOOKEEPER_VERSION}/bin/zkServer.sh start-foreground
