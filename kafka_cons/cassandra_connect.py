eed15eb3f4963a112cf69131f5e6a684bbe85315
Jakub <jakub.mojsiejuk@gmail.com>
2018-08-14 11:17:06 +0100

    kafka fixes for producer to have a longer waitime before sending to spark

--- kafka_cons/cassandra_connect.py
+++ kafka_cons/cassandra_connect.py
@@ -6,8 +6,9 @@ cluster = Cluster()
 session = cluster.connect()
 
 keyspace_init = """
-CREATE KEYSPACE emb
-  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 } """
+CREATE KEYSPACE IF NOT EXISTS emb
+  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }; 
+"""
 table_init = """
 CREATE TABLE IF NOT EXISTS emb.emb_data
 (       
@@ -22,13 +23,37 @@ CREATE TABLE IF NOT EXISTS emb.emb_data
         PRIMARY KEY (id, timestamp)
 ) WITH CLUSTERING ORDER BY (timestamp ASC);
 """
-drop = """
+
+anomaly_keyspace_init = """
+CREATE KEYSPACE IF NOT EXISTS anomal 
+                    WITH REPLICATION = {'class': 'SimpleStrategy', 
+                                        'replication_factor': 1};
+"""
+
+anomaly_table_init = """
+CREATE TABLE IF NOT EXISTS anomal.anomaly_data( 
+                      timestamp timestamp, 
+                      anomaly float, 
+                      PRIMARY KEY (timestamp)
+);
+"""
+
+drop_emb = """
     DROP TABLE IF EXISTS emb.emb_data;
 """
-session.execute(drop) # for testing
+drop_anomaly = """
+    DROP TABLE IF EXISTS anomal.anomaly_data;
+"""
+session.execute(drop_emb) # for testing
+session.execute(drop_anomaly)
 try:
     session.execute(keyspace_init)
 except cassandra.AlreadyExists:
     pass
+try:
+    session.execute(anomaly_keyspace_init)
+except cassandra.AlreadyExists:
+    pass
+session.execute(anomaly_table_init)
 session.execute(table_init)
 
